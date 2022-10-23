f2d9b96a5abe04622a6ecf6e0953dcad338b3887
Łukasz Wolski <55734622+wolski0420@users.noreply.github.com>
2022-10-23 14:59:54 +0200

    Prepared scenario class

--- src/main/java/pl/edu/agh/cea/runner/ExtendedDefaultJMetalScenario.java
+++ src/main/java/pl/edu/agh/cea/runner/ExtendedDefaultJMetalScenario.java
@@ -1,4 +1,10 @@
 package pl.edu.agh.cea.runner;
 
+/**
+ * Modified default scenario - it takes ExtendedTwoDimensionalMesh as Neighbourhood
+ */
 public class ExtendedDefaultJMetalScenario {
+    public static void main(String[] args) {
+        // @ TODO implement scenario similar to the jMetal default one but using new structure
+    }
 }
