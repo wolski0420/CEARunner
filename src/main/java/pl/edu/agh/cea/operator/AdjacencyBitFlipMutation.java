8f6dae7c11ca1bece86e135d0fd1069d30d58c7c
Łukasz Wolski <55734622+wolski0420@users.noreply.github.com>
2022-11-15 22:18:38 +0100

    Prepared ground for defining mutation using neighbour

--- src/main/java/pl/edu/agh/cea/operator/AdjacencyBitFlipMutation.java
+++ src/main/java/pl/edu/agh/cea/operator/AdjacencyBitFlipMutation.java
@@ -2,9 +2,13 @@ package pl.edu.agh.cea.operator;
 
 import org.uma.jmetal.operator.mutation.impl.BitFlipMutation;
 import org.uma.jmetal.solution.binarysolution.BinarySolution;
+import pl.edu.agh.cea.model.solution.AdjacencyBinarySolution;
 import pl.edu.agh.cea.model.solution.AdjacencySolution;
 
+import java.util.ArrayList;
+import java.util.Random;
 import java.util.Set;
+import java.util.stream.Collectors;
 
 public class AdjacencyBitFlipMutation extends BitFlipMutation implements AdjacencyMutationOperator<BinarySolution> {
     private Set<? extends AdjacencySolution<?, ?>> setOfIterAwardedSolutions;
@@ -18,5 +22,18 @@ public class AdjacencyBitFlipMutation extends BitFlipMutation implements Adjacen
         this.setOfIterAwardedSolutions = newIterAwardedSolutions;
     }
 
-    // @TODO extending mutation operator there
+    @Override
+    public void doMutation(double probability, BinarySolution solution) {
+        Set<? extends AdjacencySolution<?, ?>> awardedNeighbours = ((AdjacencyBinarySolution) solution).getNeighbours().stream()
+                .filter(setOfIterAwardedSolutions::contains)
+                .collect(Collectors.toSet());
+
+        if (!awardedNeighbours.isEmpty()) {
+            AdjacencySolution<?, ?> chosenAwardedNeighbour = new ArrayList<>(awardedNeighbours).get(new Random().nextInt(awardedNeighbours.size()));
+
+            // @TODO mutation with neighbour attendance - must be defined how it should look like
+        } else {
+            super.doMutation(probability, solution);
+        }
+    }
 }
